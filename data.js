9a324a420db6edacf0ef26bcd0f7e142c5627de5
Aleksandr Bohdanov <57062636+bohdanov90@users.noreply.github.com>
2020-03-30 09:00:49 +0300

    projects' description updated

---
+++ data.js
@@ -0,0 +1,56 @@
+var data = [
+	{
+		id: 1,
+		title: "To-Do List",
+		description: "Simple To-Do List created using Angular2+ and TypeScript. Can be used for grocery lists or simple every day tasks. Saves your data in Local Storage.",
+		linkToDemo: "https://bohdanov90.github.io/ToDoList/",
+		linkToCode: "https://github.com/bohdanov90/ToDoList/tree/no-build",
+		image: "images/works-screen/to-do-list.jpg",
+		tags: ["Angular2+", "TypeScript", "Adaptive Design"]
+	},
+	{
+		id: 2,
+		title: "Food Calculator",
+		description: "Find out how many calories you consume with my app. Search food database for items you have consumed, choose amount of servings and add them to your totals. App will calculate total calories you consumed today. Created using pure JavaScript, AJAX, BEM and SCSS. Adapted for different devices.",
+		linkToDemo: "https://bohdanov90.github.io/Food-Calculator/",
+		linkToCode: "https://github.com/bohdanov90/Food-Calculator",
+		image: "images/works-screen/food-calculator.jpg",
+		tags: ["BEM", "SCSS", "Adaptive Design", "JavaScript", "AJAX"]
+	},
+	{
+		id: 3,
+		title: "Geography Trivia",
+		description: "Trivia game created using pure JavaScript, BEM methodology and SCSS preprocessor, adapted for different devices",
+		linkToDemo: "https://bohdanov90.github.io/Geography-Trivia/",
+		linkToCode: "https://github.com/bohdanov90/Geography-Trivia",
+		image: "images/works-screen/geography-trivia.jpg",
+		tags: ["BEM", "SCSS", "Adaptive Design", "JavaScript"]
+	},
+	{
+		id: 4,
+		title: "Pig Game",
+		description: "JavaScript game for two players where you should beat your opponent by reaching Total Score faster. Game is adapted for different screens and devices. Coded using BEM methodology and SCSS preprocessor.",
+		linkToDemo: "https://bohdanov90.github.io/Pig-Game/",
+		linkToCode: "https://github.com/bohdanov90/Pig-Game",
+		image: "images/works-screen/pig-game.jpg",
+		tags: ["BEM", "SCSS", "Adaptive Design", "JavaScript"]
+	},
+	{
+		id: 5,
+		title: "Lian",
+		description: "PSD template converted into HTML5/CSS3 using Pixel Perfect approach, BEM methodology and SCSS preprocessor.",
+		linkToDemo: "https://bohdanov90.github.io/Lian/",
+		linkToCode: "https://github.com/bohdanov90/Lian",
+		image: "images/works-screen/lian.jpg",
+		tags: ["BEM", "PSD to HTML", "Pixel Perfect", "SCSS"]
+	},
+	{
+		id: 6,
+		title: "Whitesquare",
+		description: "PSD template converted into HTML5/CSS3 using HTML5 and CSS3.",
+		linkToDemo: "https://bohdanov90.github.io/Whitesquare/",
+		linkToCode: "https://github.com/bohdanov90/Whitesquare",
+		image: "images/works-screen/whitesquare.jpg",
+		tags: ["PSD to HTML"]
+	}
+];
